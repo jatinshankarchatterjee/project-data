4469257813687dbc37486d5befa36962d181d923
jatinshankarchatterjee <jatinshankarchatterjee@gmail.com>
2025-05-24 12:32:32 +0530

    remod

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
+
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
@@ -58,22 +59,14 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={
-        {
-          IconLeft: ({
-            className,
-            ...props
-          }: React.HTMLAttributes<SVGSVGElement>) => (
-            <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-          ),
-          IconRight: ({
-            className,
-            ...props
-          }: React.HTMLAttributes<SVGSVGElement>) => (
-            <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-          ),
-        } as any
-      }
+      components={{
+        IconLeft: ({ className, ...props }) => (
+          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+        ),
+        IconRight: ({ className, ...props }) => (
+          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        ),
+      }}
       {...props}
     />
   );
